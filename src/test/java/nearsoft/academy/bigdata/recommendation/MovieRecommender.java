1a4746f9d4610d86c9a0e718b8e9dd440a10e014
Nadia Pintos Velasco <nadia.pintos@Encoras-MacBook-Pro.local>
2022-04-13 21:35:28 -0500

    Reading Lines

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -1,12 +1,28 @@
 package nearsoft.academy.bigdata.recommendation;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MovieRecommender {
-    MovieRecommender(String path){
-        DataModel model = new FileDataModel(new File("/path/to/dataset.csv"));
+    MovieRecommender(String path) throws IOException {
+        File rawData = new File(path);
+        LineIterator it = FileUtils.lineIterator(rawData, "UTF-8");
+        try {
+            int freno = 0;
+            while (it.hasNext()) {
+                String line = it.nextLine();
+                // do something with line
+                System.out.println(line);
+                freno++;
+                if(freno==20) break;
+            }
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
     }
 }
